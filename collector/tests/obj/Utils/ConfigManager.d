04621e1e40c13f64e7f4656e66202208b6be8160
이경호 <kyungho@igyeonghoui-MacBookPro.local>
2025-08-10 15:37:12 +0900

    가상포인트 알람처리 구현중

--- collector/tests/obj/Utils/ConfigManager.d
+++
@@ -1,8 +0,0 @@
-obj/Utils/ConfigManager.o: ../src/Utils/ConfigManager.cpp \
- ../include/Utils/ConfigManager.h ../include/Utils/LogManager.h \
- ../include/Common/Enums.h ../include/Common/Structs.h \
- ../include/Common/BasicTypes.h ../include/Common/Enums.h \
- ../include/Common/Constants.h ../include/Common/DriverStatistics.h \
- ../include/Common/BasicTypes.h ../include/Common/Utils.h \
- ../include/Common/DriverError.h ../include/Common/IProtocolConfig.h \
- ../include/Common/ProtocolConfigs.h
