c88e3bb639a2ab014b3f5679d2cdeab485dc201a
이경호 <kyungho@igyeonghoui-MacBookPro.local>
2025-08-13 09:18:52 +0900

    가상포인트 알람처리 파이프라인 연결중

--- collector/tests/obj/Database/Entities/VirtualPointEntity.d
+++ collector/tests/obj/Database/Entities/VirtualPointEntity.d
@@ -4,14 +4,14 @@ obj/Database/Entities/VirtualPointEntity.o: \
  ../include/Database/Entities/BaseEntity.h \
  ../include/Database/DatabaseManager.h \
  ../include/Client/RedisClientImpl.h ../include/Client/RedisClient.h \
- ../include/Client/InfluxClient.h ../include/Utils/LogManager.h \
+ ../include/Utils/ConfigManager.h ../include/Utils/LogManager.h \
  ../include/Common/Enums.h ../include/Common/Structs.h \
  ../include/Common/BasicTypes.h ../include/Common/Enums.h \
  ../include/Common/Constants.h ../include/Common/DriverStatistics.h \
  ../include/Common/BasicTypes.h ../include/Common/Utils.h \
  ../include/Common/DriverError.h ../include/Common/IProtocolConfig.h \
  ../include/Common/ProtocolConfigs.h ../include/Alarm/AlarmTypes.h \
- ../include/Utils/ConfigManager.h ../include/Common/DriverStatistics.h \
+ ../include/Client/InfluxClient.h ../include/Common/DriverStatistics.h \
  ../include/Database/RepositoryFactory.h \
  ../include/Database/Repositories/VirtualPointRepository.h \
  ../include/Database/Repositories/IRepository.h \
--- collector/tests/obj/Database/Repositories/DeviceSettingsRepository.d
+++ collector/tests/obj/Database/Repositories/DeviceSettingsRepository.d
@@ -5,14 +5,14 @@ obj/Database/Repositories/DeviceSettingsRepository.o: \
  ../include/Database/DatabaseTypes.h \
  ../include/Database/DatabaseManager.h \
  ../include/Client/RedisClientImpl.h ../include/Client/RedisClient.h \
- ../include/Client/InfluxClient.h ../include/Utils/LogManager.h \
+ ../include/Utils/ConfigManager.h ../include/Utils/LogManager.h \
  ../include/Common/Enums.h ../include/Common/Structs.h \
  ../include/Common/BasicTypes.h ../include/Common/Enums.h \
  ../include/Common/Constants.h ../include/Common/DriverStatistics.h \
  ../include/Common/BasicTypes.h ../include/Common/Utils.h \
  ../include/Common/DriverError.h ../include/Common/IProtocolConfig.h \
  ../include/Common/ProtocolConfigs.h ../include/Alarm/AlarmTypes.h \
- ../include/Utils/ConfigManager.h \
+ ../include/Client/InfluxClient.h \
  ../include/Database/Entities/DeviceSettingsEntity.h \
  ../include/Database/Entities/BaseEntity.h \
  ../include/Common/DriverStatistics.h \
